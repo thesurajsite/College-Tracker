45c9c0f8b0e0a651cf205f63506430e9af439a14
surajverma <thesurajsite@gmail.com>
2024-01-09 14:41:17 +0530

    Fragment error resolved

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "com.collegetracker"
         minSdk = 27
         targetSdk = 33
-        versionCode = 2
+        versionCode = 3
         versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
