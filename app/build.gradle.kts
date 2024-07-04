d41c453562f0d9217e6c63cdfc83d19e07ef2418
Suraj <thesurajsite@gmail.com>
2024-07-04 22:32:37 +0530

    changed complete UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,7 @@ dependencies {
     implementation("androidx.room:room-ktx:2.6.1")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    implementation(kotlin("script-runtime"))
 
     //Key Alias is alias123 for Keystore
 
