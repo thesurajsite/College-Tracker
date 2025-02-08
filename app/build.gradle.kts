2ec8e45b39c13eaabe5720e281ba83e9e8b0d611
Suraj Verma <thesurajsite@gmail.com>
2025-02-08 23:04:16 +0530

    Assignmets Section Connected to Firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.collegetracker"
         minSdk = 26
         targetSdk = 35
-        versionCode = 51
-        versionName = "3.0.2"
+        versionCode = 52
+        versionName = "3.0.3"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
