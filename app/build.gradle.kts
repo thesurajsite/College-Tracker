1da5258046535f75b898298d1319def5e45f2c6b
Suraj Verma <thesurajsite@gmail.com>
2025-07-03 22:48:41 +0530

    gradle update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,15 +40,15 @@ android {
 dependencies {
 
     implementation("androidx.core:core-ktx:1.16.0")
-    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
     implementation("androidx.activity:activity:1.10.1")
-    implementation("com.google.firebase:firebase-messaging:24.1.1")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.9.0")
-    implementation("androidx.navigation:navigation-ui-ktx:2.9.0")
+    implementation("com.google.firebase:firebase-messaging:24.1.2")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.9.1")
+    implementation("androidx.navigation:navigation-ui-ktx:2.9.1")
     testImplementation("junit:junit:4.13.2")
-    implementation("androidx.room:room-ktx:2.7.1")
+    implementation("androidx.room:room-ktx:2.7.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
     implementation(kotlin("script-runtime"))
@@ -56,19 +56,19 @@ dependencies {
     //Key Alias is alias123 for Keystore
 
     //ROOM DATABSE DEPENDENCIES
-    implementation("androidx.room:room-runtime:2.7.1")
+    implementation("androidx.room:room-runtime:2.7.2")
     // To use Kotlin annotation processing tool (kapt)
-    kapt("androidx.room:room-compiler:2.7.1")
+    kapt("androidx.room:room-compiler:2.7.2")
     // optional - Kotlin Extensions and Coroutines support for Room
-    implementation("androidx.room:room-ktx:2.7.1")
+    implementation("androidx.room:room-ktx:2.7.2")
 
     //DEPENDENCIES FROM GITHUB FOR COUROURITINE
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
 
     // ViewModel
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.9.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.9.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.1")
 
     //SDP DEPENDENCIES
     implementation ("com.intuit.sdp:sdp-android:1.1.1")
@@ -82,7 +82,7 @@ dependencies {
     // Firebase
     implementation ("com.google.firebase:firebase-auth:23.2.1")
     implementation ("com.google.firebase:firebase-core:21.1.1")
-    implementation(platform("com.google.firebase:firebase-bom:33.14.0"))
+    implementation(platform("com.google.firebase:firebase-bom:33.16.0"))
     implementation("com.google.firebase:firebase-firestore:25.1.4")
     implementation("com.google.android.gms:play-services-auth:21.3.0")
 
